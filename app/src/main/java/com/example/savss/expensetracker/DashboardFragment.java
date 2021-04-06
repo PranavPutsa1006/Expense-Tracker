4d797e92d372b7873c34b334fbf0efd8e5dd1edd
Pranav Putsa <pranavputsa@gmail.com>
2021-04-06 23:07:48 +0530

    minor bug fixes

--- app/src/main/java/com/example/savss/expensetracker/DashboardFragment.java
+++ app/src/main/java/com/example/savss/expensetracker/DashboardFragment.java
@@ -157,7 +157,7 @@ public class DashboardFragment extends Fragment {
 
     private void refreashListItemsAndChart() {
         //localDatabaseHelper.getLastMonthExpenses(UserData.userID);
-        //setLastMonthPieChart();
+        setLastMonthPieChart();
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
         Date fromDate = null;
         Date toDate = null;
@@ -195,7 +195,7 @@ public class DashboardFragment extends Fragment {
         Calendar today = Calendar.getInstance();
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
         toDayTextView.setText(simpleDateFormat.format(today.getTime()));
-        today.add(Calendar.MONTH, -1);
+        today.add(Calendar.DAY_OF_MONTH, -(Calendar.DAY_OF_MONTH));
         fromDayTextView.setText(simpleDateFormat.format(today.getTime()));
 
         fromDayTextView.setOnClickListener(new View.OnClickListener() {
@@ -477,7 +477,7 @@ public class DashboardFragment extends Fragment {
     private void displayTransactionlistview() {
         transactionListView = dashboardView.findViewById(R.id.transactionListView);
         Calendar today = Calendar.getInstance();
-        today.add(Calendar.MONTH, -1);
+        today.add(Calendar.DAY_OF_MONTH, -(Calendar.DAY_OF_MONTH));
 
         transactionListViewAdapter = new TransactionListViewAdapter(localDatabaseHelper.getTransactionData(UserData.userID, today.getTime(), Calendar.getInstance().getTime()));
         transactionListView.setAdapter(transactionListViewAdapter);
